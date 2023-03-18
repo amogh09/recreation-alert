a767272a15d4d7b267414cdf0aa072add75afcff
Amogh Rathore <amoghdroid09@gmail.com>
2023-03-18 12:22:02 -0700

    Simplify main

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import CLI (Args (..), opts)
 import Control.Monad.Reader (ReaderT (runReaderT))
+import Data.Functor.Contravariant (Predicate (Predicate))
 import Data.List ((\\))
 import Data.Time.Calendar
 import Env (loadEnv)
@@ -11,6 +12,19 @@ import Recreation.Core.Types (Campground (Campground))
 import Recreation.Usecase.Availability (go)
 import Text.Printf (printf)
 
+cougarRock :: Predicate Day -> Campground
+cougarRock = Campground "232466" "Cougar Rock" (Predicate $ const True)
+
+kalaloch :: Predicate Day -> Campground
+kalaloch =
+  Campground "232464" "Kalaloch" (siteIn [printf "A%03d" i | i <- [11 .. 28 :: Int] \\ [24]])
+
+ohanapecosh :: Predicate Day -> Campground
+ohanapecosh = Campground "232465" "Ohanapecosh Campground" (Predicate $ const True)
+
+newHalem :: Predicate Day -> Campground
+newHalem = Campground "234060" "Newhalem Campground" (Predicate $ const True)
+
 main :: IO ()
 main = do
   args <- execParser opts
@@ -19,27 +33,7 @@ main = do
   env <- loadEnv
   runReaderT
     ( go
-        [ -- Campground
-          --   "232466"
-          --   "Cougar Rock"
-          --   (Predicate $ const True)
-          --   dayPred,
-          Campground
-            "232464"
-            "Kalaloch"
-            (siteIn [printf "A%03d" i | i <- [11 .. 28 :: Int] \\ [24]])
-            dayPred
-            -- Campground
-            --   "232465"
-            --   "Ohanapecosh Campground"
-            --   (Predicate $ const True)
-            --   dayPred,
-            -- Campground
-            --   "234060"
-            --   "Newhalem Campground"
-            --   (Predicate $ const True)
-            --   dayPred
-        ]
+        [c dayPred | c <- []]
         args.startDate
         args.endDate
     )
