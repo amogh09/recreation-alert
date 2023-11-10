2fa2c51c79e1565db5bacc2f1351cc53e1ecb0ed
Amogh Rathore <amoghdroid09@gmail.com>
2023-11-10 08:45:09 -0800

    Make shell.nix reproducible

--- shell.nix
+++ shell.nix
@@ -1,10 +1,12 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/982816eaeefe3b7c5d51afc2ba777e627a98c4ac.tar.gz";
+    sha256 = "sha256:17g48gx9ya72d5gq1imzk94k0m1vzf5ghwzawh0craqjil7yc0m7";
+}) {} }:
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [
-    (haskellPackages.ghcWithPackages
+  packages = with pkgs; [
+    (haskell.packages.ghc927.ghcWithPackages
       (pkgs: with pkgs;
       [ cabal-install haskell-language-server ormolu ]))
-    zlib
   ];
   shellHook = ''
     export PATH="$HOME/.cabal/bin:$PATH"
